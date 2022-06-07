3ea03b3ad76fec4212e9d8b5e685968b412adb08
moses ngige <moses32ngigz@gmail.com>
2022-06-07 10:05:58 +0300

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 let num1 = 31;
 let num2 = 2;
-let multiply = $[num1 * num2];
+let multiply = num1 * num2;
+
+let random =  Math.floor(Math.random() * 10) ; 
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+let numbers = [2,3,20];
+let max = console.log( math.max(2,3,20) ) ;
